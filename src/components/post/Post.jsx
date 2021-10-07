d7006cf9b20373bf23835ff71db8dade97820748
Mohamed-tahiri <mhdtahiri01@gmail.com>
2021-10-07 16:10:48 +0100

    Like system

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,18 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export default function Post({ post }) {
-    
+
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked ] = useState(false);
+
+    const likeHandler = ()=>{
+        setLike(isLiked ? like-1 : like+1 )
+        setIsLiked(!isLiked)
+    }
+
     return (
         <div className="post">
             <div className="postWrapper">
@@ -33,9 +42,19 @@ export default function Post({ post }) {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="/assets/like.png" alt="like" />
-                        <img className="likeIcon" src="/assets/heart.png" alt="heart" />
-                        <span className="postLikeCounter">{post.like} people like it</span>
+                        <img 
+                            className="likeIcon" 
+                            src="/assets/like.png" 
+                            onClick={likeHandler}
+                            alt="like" 
+                        />
+                        <img 
+                            className="likeIcon" 
+                            src="/assets/heart.png" 
+                            onClick={likeHandler}
+                            alt="heart" 
+                        />
+                        <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{post.comment} comments</span>
